60e326b8fe484aa3fb86ba7f17ac23a69bf79f1f
poonam sandip patel <sarbhadi@gmail.com>
2022-05-18 15:30:32 +0530

    ok

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,11 +1,28 @@
-// const urlModel = require("../model/Url Model")
+const urlModel = require("../model/urlModel")
+
 
 const createUrl = async function(req,res){
     try{
         let data = req.body
-        if(Object.keys(data).length == 0) return res.status(400).send({status:false, message:"please provide data in body"})
-        if(!data.longUrl) return res.status(400).send({status: false, message:""})
-        console.log(data)
+if(Object.keys(data).length == 0) return res.status(400).send({status:false, message:"please provide data"})
+       if (!data.longUrl) return res.status(400).send({status:false, message:"only accept longUrl please check again"})
+
+       
+
+
+    }
+    catch(err){
+        res.status(500).send({status:false, err:err.message})
+    }
+}
+
+
+const getUrl = async function(req,res){
+    try{
+        let data = req.body
+
+        if(!data.longUrl) return res.status(400).send({status: false, message:"please provide longUrl"})
+
     }
     catch(err){
         res.status(500).send({status:false, err:err.message})
@@ -13,3 +30,4 @@ const createUrl = async function(req,res){
 }
 
 module.exports.createUrl = createUrl
+module.exports.getUrl = getUrl
